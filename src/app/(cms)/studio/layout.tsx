e49e84eb1d55cb0eec39b00a0ffe1ec286a8c91d
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-12-26 16:17:17 +0300

    setup the cms in the frontend

---
+++ src/app/(cms)/studio/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Hotel Management App",
+  description: "Discover the best hotel rooms",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
