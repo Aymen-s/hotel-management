423386c7a3c3427a8a69855670c094141a1a8878
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2024-12-26 19:54:24 +0300

    creating the verification token schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,12 @@ import account from "./account";
 import booking from "./booking";
 import hotelRoom from "./hotelRoom";
 import user from "./user";
+import verificationToken from "./verificationToken";
 
-export const schemaTypes = [user, account, booking, hotelRoom];
+export const schemaTypes = [
+  user,
+  account,
+  booking,
+  hotelRoom,
+  verificationToken,
+];
