bde69aa2b4562dc37e9c2a5f7c100aba8dc70065
Aymen Sileshi <aymen.sileshi@aau.edu.et>
2025-01-07 00:36:00 +0300

    completing rendering the featured rooms on the hero section

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: [
+      "lh3.googleusercontent.com",
+      "plus.unsplash.com",
+      "images.unsplash.com",
+    ],
   },
 };
 
